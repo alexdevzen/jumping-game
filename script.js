197a4b2af4c987279761101f268fea866c0b4354
alexdevzen <alex.devzen@gmail.com>
2024-08-20 16:47:02 -0400

    Jump function

---
+++ script.js
@@ -0,0 +1,37 @@
+// References to DOM
+const character = document.getElementById('character');
+const obstacle = document.getElementById('obstacle');
+const scoreElement = document.getElementById('score');
+
+//State variables
+let isJumping = false;
+let score = 0;
+
+function jump() {
+    if (isJumping) return;
+    isJumping = true;
+    let jumpCount = 0;
+    let jumpInterval = setInterval(() => {
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
+
+        //Ascending
+        if (jumpCount < 20) {
+            characterTop += 5;
+            character.style.bottom = characterTop + 'px';
+            jumpCount++;
+        }
+        //Descending
+        else {
+            clearInterval(jumpInterval);
+            let fallInterval = setInterval(() => {
+                if (characterTop <= 0) {
+                    clearInterval(fallInterval);
+                    isJumping = false;
+                } else {
+                    characterTop -= 5;
+                    character.style.bottom = characterTop + 'px';
+                }
+            }, 20);
+        }
+    }, 20);
+}
